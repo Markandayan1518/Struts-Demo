dc9dfeb6b70afc8da0082644a3cb6f8f54a8049d
Markandayan Perumandi <marklevy1518@gmail.com>
2016-06-15 11:42:59 +0530

    Added Sample data

---
+++ Struts Tutorial/Sampledata.sql
@@ -0,0 +1,5 @@
+create table account ( username VARCHAR(20), password VARCHAR(20), emailid VARCHAR(50), fullname VARCHAR(30), gender VARCHAR(6) );
+
+insert into account values ('Mark', 'login', 'markandayan.p@zohocorp.com', 'Markandayan', 'Male');
+
+select * from account;
